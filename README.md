ccfcc508423f6dee0622f42eab61f8ca19b748ca
Comus Bala <comusbala96.com>
2025-07-31 00:10:56 +0530

    ready to first publish

---
+++ README.md
@@ -0,0 +1,27 @@
+# A npm package of lensasia_app for OrianSoft.
+
+## Installation
+
+```npm
+npm i @oriansoft/lensasia_app
+```
+
+## Or
+
+```yarn
+yarn add @oriansoft/lensasia_app
+```
+
+## Import
+
+```js
+import '@oriansoft/lensasia_app';
+```
+
+## Credits
+
+-   [Comus Bala](https://github.com/comusbala96)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
--- src/admin/settings/logo.js
+++ src/admin/settings/logo.js
@@ -1,17 +1,17 @@
 import { ajaxRequest } from '@oriansoft/utils';
     
 $(document).ready(function () {
+    $.validator.addMethod('requiredIfOthersEmpty', function (value, element, fields) {
+        let isAnyOtherFilled = fields.some(function (selector) {
+            return $.trim($(selector).val()) !== '';
+        });
+        return isAnyOtherFilled || $.trim(value) !== '';
+    }, 'At least one field is required.');
     if ($('#frmUpdateLogoSettings').length > 0) {
         let rules = {
-            logo: {
-                required: true,
-            },
-            favicon: {
-                required: true,
-            },
-            auth_logo: {
-                required: true,
-            },
+            logo: { requiredIfOthersEmpty: ['#favicon', '#auth_logo'] },
+            favicon: { requiredIfOthersEmpty: ['#logo', '#auth_logo'] },
+            auth_logo: { requiredIfOthersEmpty: ['#logo', '#favicon'] }
         };
         ajaxRequest({
             element: 'frmUpdateLogoSettings',
